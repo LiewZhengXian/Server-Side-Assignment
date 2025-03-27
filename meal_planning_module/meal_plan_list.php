973648ed459525d14dcff829d0c4407d8ca6e531
JosephTanKaiJun <kaijuntan423@1utar.my>
2025-03-27 15:36:58 +0800

    add display and modify list

--- meal_planning_module/meal_plan_list.php
+++ meal_planning_module/meal_plan_list.php
@@ -87,6 +87,7 @@ $meal_times = ["Breakfast", "Lunch", "Dinner"];
                             <li><a class="dropdown-item active" href="meal_plan_add.php">Plan a Meal</a></li>
                             <li><a class="dropdown-item" href="meal_plan_list.php">View Schedule</a></li>
                             <li><a class="dropdown-item active" href="../meal_template_module/list_templates.php">Manage Templates</a></li>
+                            <li><a class="dropdown-item" href="meal_plan_display.php">Display Schedule Table</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
