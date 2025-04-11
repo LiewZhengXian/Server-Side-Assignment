e98920517da425314e13478a4223bb9032caac6e
Liew Zheng Xian <zhengxianliew107@gmail.com>
2025-04-11 14:44:33 +0800

    Navbar is done

---
+++ navbar.php
@@ -0,0 +1,41 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Recipe Hub</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/Server-Side-Assignment/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Server-Side-Assignment/recipe_management_module/recipe.php">Recipes</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="mealPlanningDropdown" role="button"
+                            data-bs-toggle="dropdown">
+                            Meal Planning
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Plan a Meal</a></li>
+                            <li><a class="dropdown-item" href="#">View Schedule</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Server-Side-Assignment/community_module/Community.php">Community</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Competitions</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/Server-Side-Assignment/user_module/logout.php">Logout</a>
+
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
