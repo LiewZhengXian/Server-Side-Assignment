3a405b91922d1802c4d21aa4d962ec8afe8623d7
Luci6n <jw20020301@gmail.com>
2025-04-16 01:36:53 +0800

    even better refactoring noice

--- recipe_management_module/recipe.php
+++ recipe_management_module/recipe.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
 
-// Determine the redirection based on user role
-if (!empty($_SESSION['isAdmin'])) {
-    $redirectPage = ($_SESSION['isAdmin'] == 1) ? 'recipe_admin.php' : 'recipe_user.php';
-} else {
-    $redirectPage = 'recipe_user.php';
-}
+$redirectPage = (isset($_SESSION['isAdmin']) && $_SESSION["isAdmin"] == 1) 
+    ? 'recipe_admin.php'
+    : 'recipe_user.php';
 
 // Redirect to the appropriate page
 header("Location: $redirectPage");
