c9816b21fec44ed9bda31d8dcbc19c0447e820e2
Luci6n <jw20020301@gmail.com>
2025-04-16 01:28:15 +0800

    refactored the code

--- recipe_management_module/recipe.php
+++ recipe_management_module/recipe.php
@@ -1,18 +1,13 @@
 <?php
 session_start();
-// Check if the user is logged in and if isAdmin is set
-if (isset($_SESSION['isAdmin'])) {
-    if ($_SESSION['isAdmin'] == 1) {
-        // Redirect to the admin recipe page
-        header("Location: recipe_admin.php");
-        exit();
-    } else {
-        // Redirect to the user recipe page
-        header("Location: recipe_user.php");
-        exit();
-    }
+
+// Determine the redirection based on user role
+if (!empty($_SESSION['isAdmin'])) {
+    $redirectPage = ($_SESSION['isAdmin'] == 1) ? 'recipe_admin.php' : 'recipe_user.php';
 } else {
-    // Redirect to the user recipe page
-    header("Location: recipe_user.php");
-    exit();
+    $redirectPage = 'recipe_user.php';
 }
+
+// Redirect to the appropriate page
+header("Location: $redirectPage");
+exit();
