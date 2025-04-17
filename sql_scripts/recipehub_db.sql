634dcfba84142ce2740b3677ceb8b6e30d7b6af2
Joseph_423 <kaijuntan423@1utar.my>
2025-04-17 17:01:08 +0800

    Update recipehub_db.sql
    modify the meal plan inserted data

--- sql_scripts/recipehub_db.sql
+++ sql_scripts/recipehub_db.sql
@@ -41,16 +41,6 @@ CREATE TABLE `meal_plans` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ;
 
---
--- Dumping data for table `meal_plans`
---
-
-INSERT INTO `meal_plans` (`meal_id`, `user_id`, `meal_name`, `meal_date`, `meal_time`, `meal_type`, `created_at`, `recipe_id`, `custom_meal`, `duration`, `updated_at`) VALUES
-(4, 6, 'Fish', '2025-03-28', 'Lunch', 'recipe', '2025-03-12 13:49:16', 4, NULL, 5, '2025-03-26 08:02:14'),
-(5, 6, 'Bear', '2025-03-26', 'Dinner', 'custom', '2025-03-12 13:56:35', NULL, 'Bear and biscuit', 9, '2025-03-26 07:43:05'),
-(6, 6, 'Fish and me ', '2025-04-12', 'Breakfast', 'recipe', '2025-04-12 13:54:04', 3, NULL, 9, NULL),
-(7, 6, 'The cube', '2025-04-07', 'Lunch', 'custom', '2025-04-12 13:54:32', NULL, 'Eat rubic cube', 21, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -65,14 +55,6 @@ CREATE TABLE `meal_template` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `meal_template`
---
-
-INSERT INTO `meal_template` (`template_id`, `user_id`, `template_name`, `description`, `created_at`, `updated_at`) VALUES
-(4, 6, 'B', 'B', '2025-03-27 05:20:52', NULL),
-(7, 6, 'Sushi mentai', 'Sushi mentai super good ', '2025-04-12 09:06:40', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,54 +72,6 @@ CREATE TABLE `meal_template_details` (
   `recipe_id` int(11) DEFAULT NULL,
   `custom_meal` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `meal_template_details`
---
-
-INSERT INTO `meal_template_details` (`template_detail_id`, `template_id`, `day_of_week`, `meal_time`, `meal_name`, `meal_type`, `recipe_id`, `custom_meal`) VALUES
-(85, 4, 'Monday', 'Breakfast', 'B', 'custom', NULL, 'dadawdaw'),
-(86, 4, 'Monday', 'Lunch', 'B', 'recipe', 4, NULL),
-(87, 4, 'Monday', 'Dinner', 'B', 'recipe', 2, NULL),
-(88, 4, 'Tuesday', 'Breakfast', 'B', 'recipe', 2, NULL),
-(89, 4, 'Tuesday', 'Lunch', 'B', 'recipe', 1, NULL),
-(90, 4, 'Tuesday', 'Dinner', 'B', 'recipe', 1, NULL),
-(91, 4, 'Wednesday', 'Breakfast', 'B', 'recipe', 4, NULL),
-(92, 4, 'Wednesday', 'Lunch', 'B', 'recipe', 4, NULL),
-(93, 4, 'Wednesday', 'Dinner', 'B', 'recipe', 2, NULL),
-(94, 4, 'Thursday', 'Breakfast', 'B', 'recipe', 1, NULL),
-(95, 4, 'Thursday', 'Lunch', 'B', 'recipe', 2, NULL),
-(96, 4, 'Thursday', 'Dinner', 'B', 'recipe', 2, NULL),
-(97, 4, 'Friday', 'Breakfast', 'B', 'recipe', 2, NULL),
-(98, 4, 'Friday', 'Lunch', 'B', 'recipe', 3, NULL),
-(99, 4, 'Friday', 'Dinner', 'B', 'recipe', 3, NULL),
-(100, 4, 'Saturday', 'Breakfast', 'B', 'recipe', 2, NULL),
-(101, 4, 'Saturday', 'Lunch', 'B', 'recipe', 2, NULL),
-(102, 4, 'Saturday', 'Dinner', 'B', 'recipe', 3, NULL),
-(103, 4, 'Sunday', 'Breakfast', 'B', 'recipe', 2, NULL),
-(104, 4, 'Sunday', 'Lunch', 'B', 'recipe', 1, NULL),
-(105, 4, 'Sunday', 'Dinner', 'B', 'recipe', 3, NULL),
-(148, 7, 'Monday', 'Breakfast', 'A', 'recipe', 3, NULL),
-(149, 7, 'Monday', 'Lunch', 'A', 'recipe', 1, NULL),
-(150, 7, 'Monday', 'Dinner', 'A', 'recipe', 4, NULL),
-(151, 7, 'Tuesday', 'Breakfast', 'A', 'recipe', 3, NULL),
-(152, 7, 'Tuesday', 'Lunch', 'A', 'recipe', 3, NULL),
-(153, 7, 'Tuesday', 'Dinner', 'A', 'recipe', 3, NULL),
-(154, 7, 'Wednesday', 'Breakfast', 'A', 'recipe', 1, NULL),
-(155, 7, 'Wednesday', 'Lunch', 'A', 'recipe', 2, NULL),
-(156, 7, 'Wednesday', 'Dinner', 'A', 'recipe', 5, NULL),
-(157, 7, 'Thursday', 'Breakfast', 'A', 'recipe', 1, NULL),
-(158, 7, 'Thursday', 'Lunch', 'A', 'recipe', 3, NULL),
-(159, 7, 'Thursday', 'Dinner', 'A', 'recipe', 4, NULL),
-(160, 7, 'Friday', 'Breakfast', 'A', 'recipe', 3, NULL),
-(161, 7, 'Friday', 'Lunch', 'A', 'recipe', 2, NULL),
-(162, 7, 'Friday', 'Dinner', 'A', 'recipe', 3, NULL),
-(163, 7, 'Saturday', 'Breakfast', 'A', 'recipe', 3, NULL),
-(164, 7, 'Saturday', 'Lunch', 'A', 'recipe', 2, NULL),
-(165, 7, 'Saturday', 'Dinner', 'A', 'recipe', 5, NULL),
-(166, 7, 'Sunday', 'Breakfast', 'A', 'recipe', 4, NULL),
-(167, 7, 'Sunday', 'Lunch', 'A', 'recipe', 4, NULL),
-(168, 7, 'Sunday', 'Dinner', 'A', 'recipe', 3, NULL);
 
 --
 -- Indexes for dumped tables
